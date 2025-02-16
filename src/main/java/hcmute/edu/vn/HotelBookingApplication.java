34ebf3016adf518e62de111bf92459a2bd7bdd17
hieuzeus <hieu01bdvn@gmail.com>
2025-02-16 10:51:41 +0700

    init project

---
+++ src/main/java/hcmute/edu/vn/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package hcmute.edu.vn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+}
