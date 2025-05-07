f2f02fcd17da2b6dc7cc723034d32e75ac479fd0
devzeus <hieu01bdvn@gmail.com>
2025-05-07 08:36:44 +0700

    update validate and README additionally

--- src/main/java/hcmute/edu/vn/dto/request/SignupRequest.java
+++ src/main/java/hcmute/edu/vn/dto/request/SignupRequest.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class SignupRequest {
     private String email;
+    private String phone;
     private String password;
     private String confirmPassword;
     private String firstName;
